88c55de0f01625787c89b57e78992f1a6ab68e47
Snuffy <stuffyxd@gmail.com>
2023-11-11 17:08:56 -0300

    Agregar logica para hacer uso de los OCRs disponibles

--- lambda/ProcessImageOCR/main.js
+++ lambda/ProcessImageOCR/main.js
@@ -1,11 +1,35 @@
+const OCR_KEYS = {
+  LLM: "llm",
+};
+
 exports.handler = async function (event, context) {
   const { ProcessImageOCR } = event;
-
-  const { count } = ProcessImageOCR;
-  const OCRS_AVAILABLE = [];
+  const { count, data } = ProcessImageOCR;
+  const OCRS_AVAILABLE = [OCR_KEYS.LLM];
+  const extractedData = requestDataToOcr(OCRS_AVAILABLE[count]);
 
   return {
     count: count + 1,
-    completed: count === OCRS_AVAILABLE.length,
+    completed:
+      count == OCRS_AVAILABLE.length || checkIfWeExtractedAllFields(data),
+    data: Object.assign(data, extractedData),
   };
 };
+
+function checkIfWeExtractedAllFields(data) {
+  for (const [key, value] of Object.entries(data)) {
+    if (value === -1) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function requestDataToOcr(ocr) {
+  switch (ocr) {
+    case OCR_KEYS.LLM: {
+      return {};
+    }
+  }
+}
